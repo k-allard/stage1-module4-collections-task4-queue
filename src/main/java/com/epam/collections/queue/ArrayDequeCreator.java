b5eaf98d0d7c22b70092695df9f8be242fa64ff6
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-04-24 19:15:12 +0400

    Done tasks

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -5,6 +5,25 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
+    private final ArrayDeque<Integer> res = new ArrayDeque<>();
+
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        res.add(firstQueue.remove());
+        res.add(firstQueue.remove());
+        res.add(secondQueue.remove());
+        res.add(secondQueue.remove());
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            oneStep(firstQueue);
+            oneStep(secondQueue);
+        }
+        return res;
+    }
+
+    private void oneStep(Queue<Integer> queue) {
+        queue.add(res.removeLast());
+        res.add(queue.remove());
+        if (queue.isEmpty())
+            return;
+        res.add(queue.remove());
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,21 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++)
+            deque.add(i);
+        while (!deque.isEmpty()) {
+            for (int j = 0; j < everyDishNumberToEat - 1; j++)
+                deque.add(deque.remove());
+            res.add(deque.remove());
+        }
+        return res;
     }
 }
